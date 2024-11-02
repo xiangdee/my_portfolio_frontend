46496f0ab9288792bac53d9130044ef0efb7dd41
Emmanuel Francis <youngelmatee@gmail.com>
2024-11-02 06:25:19 +0100

    made the projects view page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,11 @@ import { ScrollDownButton } from '../homeCompnents/ScrollDownButton';
 import Link from 'next/link';
 import ProjectsCompoent from '@/components/ProjectsCompoent';
 import ContactMe from '@/homeCompnents/ContactMe';
+import TimelineComponent from '@/components/TimelineComponent';
 
  async function getProjects() {
   const projects = await client.fetch(
-    `*[_type == 'portfolio2'] {
+    `*[_type == 'portfolio'] {
       title,
       category,
       slug,
@@ -350,62 +351,7 @@ export default async function Page() {
                         <h5 className='text-lg'>Working Experience</h5>
                     </div>
                 </div>
-                <div className="timeline mt-5 md:ml-32">
-                    <div className="event">
-                      <div className='eventDate'>
-                          <h4>2021 - 2023</h4>
-                      </div>
-                      <div className="eventContent">
-                        <h3 className='text-xl text-primary'>Software Engineer</h3>
-                        <h4 className='text-xl'>DAV_INCHI INVESTMENT LIMITED</h4>
-                        <p className='text-sm'>
-                        As a software engineer at Dav_inchi International, I played a pivotal role in developing and maintaining a 
-                        comprehensive on-premise distributed system using Windows Server and Python Flask. 
-
-                        One notable accomplishment was the successful integration of Salesforce API into the on-premise portal, enhancing 
-                        the platform&lsquo;s functionalities. Additionally, I contributed to the company&lsquo;s online presence by designing, 
-                        developing, and deploying a user-friendly WordPress website. 
- 
-                        </p>
-                      </div>
-                    </div>
-
-                    <div className="event">
-                      <div className='eventDate'>
-                          <h4>2022 - 2023</h4>
-                      </div>
-                      <div className="eventContent">
-                        <h3 className='text-xl text-primary'>Back End Developer</h3>
-                        <h4 className='text-xl'>Pykup</h4>
-                        <p className='text-sm'>
-                          In the backend operations of Pykup, I played a key role in integrating third-party APIs, addressing scalability and 
-                          security concerns, and writing unit tests. My responsibilities included 
-                          creating documentation using tools like Postman and Swagger, and ensuring code adhered to standards like 
-                          SOLID and DRY principles. I effectively communicated software requirements across teams and managed tasks 
-                          following the waterfall model on Trello. I actively contributed to the development of authentication in a 
-                          microservices environment using NestJS, performing daily business logic implementation, and conducting code reviews using Git version control. 
-                        </p>
-                      </div>
-                    </div>
-
-                    <div className="event">
-                      <div className='eventDate'>
-                          <h4>2023 - 2024</h4>
-                      </div>
-                      <div className="eventContent">
-                        <h3 className='text-xl text-primary'>Fullstack Engineer</h3>
-                        <h4 className='text-xl'>Takerslogistics</h4>
-                        <p className='text-sm'>
-                          Developed a robust delivery hailing service, Takers Logistics, involving the creation of React Native apps 
-                          for users and drivers, and a visually appealing landing page using Next.js and Tailwind CSS. 
-                          Implemented a microservices architecture with PHP and NestJS web sockets for efficient communication, 
-                          addressing security and concurrency challenges. Deployed the system using Docker and GitHub Actions on
-                           Google Cloud Platform, showcasing proficiency in infrastructure and deployment practices. 
-                        </p>
-                      </div>
-                    </div>
-                        
-                </div>
+                <TimelineComponent/>
               </div>
           </div>
         </section>
---
+++ src/components/TimelineComponent.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+export default function TimelineComponent() {
+  return (
+    <div className="timeline mt-5 md:ml-32">
+                    <div className="event">
+                      <div className='eventDate'>
+                          <h4>2021 - 2023</h4>
+                      </div>
+                      <div className="eventContent">
+                        <h3 className='text-xl text-primary'>Software Engineer</h3>
+                        <h4 className='text-xl'>DAV_INCHI INVESTMENT LIMITED</h4>
+                        <p className='text-sm'>
+                        As a software engineer at Dav_inchi International, I played a pivotal role in developing and maintaining a 
+                        comprehensive on-premise distributed system using Windows Server and Python Flask. 
+
+                        One notable accomplishment was the successful integration of Salesforce API into the on-premise portal, enhancing 
+                        the platform&lsquo;s functionalities. Additionally, I contributed to the company&lsquo;s online presence by designing, 
+                        developing, and deploying a user-friendly WordPress website. 
+ 
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="event">
+                      <div className='eventDate'>
+                          <h4>2022 - 2023</h4>
+                      </div>
+                      <div className="eventContent">
+                        <h3 className='text-xl text-primary'>Back End Developer</h3>
+                        <h4 className='text-xl'>Pykup</h4>
+                        <p className='text-sm'>
+                          In the backend operations of Pykup, I played a key role in integrating third-party APIs, addressing scalability and 
+                          security concerns, and writing unit tests. My responsibilities included 
+                          creating documentation using tools like Postman and Swagger, and ensuring code adhered to standards like 
+                          SOLID and DRY principles. I effectively communicated software requirements across teams and managed tasks 
+                          following the waterfall model on Trello. I actively contributed to the development of authentication in a 
+                          microservices environment using NestJS, performing daily business logic implementation, and conducting code reviews using Git version control. 
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="event">
+                      <div className='eventDate'>
+                          <h4>2023 - 2024</h4>
+                      </div>
+                      <div className="eventContent">
+                        <h3 className='text-xl text-primary'>Fullstack Engineer</h3>
+                        <h4 className='text-xl'>Takerslogistics</h4>
+                        <p className='text-sm'>
+                          Developed a robust delivery hailing service, Takers Logistics, involving the creation of React Native apps 
+                          for users and drivers, and a visually appealing landing page using Next.js and Tailwind CSS. 
+                          Implemented a microservices architecture with PHP and NestJS web sockets for efficient communication, 
+                          addressing security and concurrency challenges. Deployed the system using Docker and GitHub Actions on
+                           Google Cloud Platform, showcasing proficiency in infrastructure and deployment practices. 
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="event">
+                      <div className='eventDate'>
+                          <h4>2024 - present</h4>
+                      </div>
+                      <div className="eventContent">
+                        <h3 className='text-xl text-primary'>Fullstack Engineer</h3>
+                        <h4 className='text-xl'>Surfmeal</h4>
+                        <p className='text-sm'>
+                        Developed a comprehensive food delivery platform, Surfmeal, creating a user-friendly 
+                        React Native app for customers and a streamlined management interface for vendors. 
+                        Built an engaging, responsive Next.js landing page with Tailwind CSS, showcasing the app&rsquo;s 
+                        unique features and user benefits. Leveraged NestJS and Prisma for backend services, implementing 
+                        a robust API that handles user authentication, store management, and real-time order tracking. 
+                        Integrated diverse payment methods, including wallets and gift cards, enhancing in-app transaction 
+                        convenience. Architected and deployed the platform on AWS using Docker and GitHub Actions, 
+                        ensuring scalability, security, and smooth updates across services.
+
+
+                        </p>
+                      </div>
+                    </div>
+                        
+                </div>
+  )
+}
